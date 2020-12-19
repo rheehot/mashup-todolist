aecd336a2aabb52467f5e846d0aebb7118632c97
peter L <peter.l@uvaper.com>
2020-12-19 11:33:21 +0900

    study #2 manage-state

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -26,10 +27,12 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  const todos = useTodoState();
+  console.log(todos);
   return (
     <TodoHeadBlock>
-      <h1>2020년 12월 19일</h1>
-      <div className="day">토요일</div>
+      <h1>2019년 7월 10일</h1>
+      <div className="day">수요일</div>
       <div className="tasks-left">할 일 2개 남음</div>
     </TodoHeadBlock>
   );
